a3be9367e37d93d9ab03d6c612098e06456783c4
Edward <hu15067170411@163.com>
2020-04-09 17:58:12 +0800

    20200409

--- Edar/pages/order/order.js
+++ Edar/pages/order/order.js
@@ -9,33 +9,33 @@ Page({
     tabList: [
       {
         "text": "全部",
-        "iconPath": "/resources/img/home.png",
-        "selectedIconPath": "/resources/img/homed.png"
+        "iconPath": "/resources/img/all.png",
+        "selectedIconPath": "/resources/img/alled.png"
       },
       {
         "text": "待上门",
-        "iconPath": "/resources/img/log.png",
-        "selectedIconPath": "/resources/img/loged.png"
+        "iconPath": "/resources/img/sand.png",
+        "selectedIconPath": "/resources/img/sanded.png"
       },
       {
         "text": "维修中",
-        "iconPath": "/resources/img/log.png",
-        "selectedIconPath": "/resources/img/loged.png"
+        "iconPath": "/resources/img/fix.png",
+        "selectedIconPath": "/resources/img/fixed.png"
       },
       {
         "text": "待验收",
-        "iconPath": "/resources/img/log.png",
-        "selectedIconPath": "/resources/img/loged.png"
+        "iconPath": "/resources/img/wait.png",
+        "selectedIconPath": "/resources/img/waited.png"
       },
       {
         "text": "待支付",
-        "iconPath": "/resources/img/log.png",
-        "selectedIconPath": "/resources/img/loged.png"
+        "iconPath": "/resources/img/waitPay.png",
+        "selectedIconPath": "/resources/img/waitPayed.png"
       },
       {
         "text": "待评价",
-        "iconPath": "/resources/img/log.png",
-        "selectedIconPath": "/resources/img/loged.png"
+        "iconPath": "/resources/img/assess.png",
+        "selectedIconPath": "/resources/img/assessed.png"
       }
     ],
     orderArr: [
